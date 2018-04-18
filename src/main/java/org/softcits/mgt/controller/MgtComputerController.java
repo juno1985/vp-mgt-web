21f6d0c0f830b406f2956cf230d79b504c9761df
Jun <wangzhen_tju@126.com>
2018-04-18 14:30:55 +0800

    jQuery on函数绑定事件可以选择子元素，bind函数则不能

--- src/main/java/org/softcits/mgt/controller/MgtComputerController.java
+++ src/main/java/org/softcits/mgt/controller/MgtComputerController.java
@@ -1,12 +1,17 @@
 package org.softcits.mgt.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.softcits.mgt.service.MgtComputerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/computer")
@@ -14,8 +19,21 @@ public class MgtComputerController {
 
 	@Autowired
 	private MgtComputerService mgtComputerService;
+	
+	@Value("${pc.upload.path}")
+	private String pc_upload_path;
+	
 	@RequestMapping(path="/list",method=RequestMethod.GET)
 	public ResponseEntity<String> getAll(){
 		return new ResponseEntity<>(mgtComputerService.getComputers(),HttpStatus.OK);
+	}
+	
+	@RequestMapping(path="/add",method=RequestMethod.POST)
+	public ResponseEntity<String> addComputer(@RequestParam String tradeMark, @RequestParam String price, @RequestParam("pic")MultipartFile multipartFile, HttpServletRequest req){
+		
+		System.out.println(tradeMark + ' ' + price + ' ' + multipartFile.getOriginalFilename());
+		String realPath = req.getSession().getServletContext().getRealPath(pc_upload_path);
+		System.out.println(realPath);
+		return new ResponseEntity<String>("Success!", HttpStatus.OK);
 	}
 }
--- static/js/pc_admin.js
+++ static/js/pc_admin.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
-	
 	pc_list();
-	
 	$(".panel-heading").click(function(e) {
 		console.log(e);
 		/* 切换折叠指示图标 */
 		$(this).find("span").toggleClass("glyphicon-chevron-down");
 		$(this).find("span").toggleClass("glyphicon-chevron-up");
 	});
+	$('#mgt_content').on('click','.pcFormSubmit',pc_add_submit);
 });
 function pc_list(){
-	$('#pcTable').children().remove();
+	$('#mgt_content').children().remove();
 	$.ajax({
 		url: "/mgt/computer/list",
 		type: "GET",
 		dataType: "json",
 		success:function(data){
-			var content = "<thead><tr><th>商品id</th><th>商品名称</th><th>商品价格</th><th>更新</th><th>删除</th></tr></thead>";
+			var content = "<table id="+'"pcTable"'+" class="+'"table table-condensed"'+"><tbody>";
+			content += "<thead><tr><th>商品id</th><th>商品名称</th><th>商品价格</th><th>更新</th><th>删除</th></tr></thead>";
 			// v-value, i-index
 			data.forEach(function(v,i){
 				/*
@@ -28,14 +28,30 @@ function pc_list(){
 							'<td><a href="/mgt/computer/delete/'+v.id+'">删除</a></td>'+
 							"</tr>";
 			}); 
-			$('#pcTable').append("<tbody>"+content+"</tbody>");
+			content += "</table></tbody>";
+			$('#mgt_content').append(content);
 		}
 	});	
 }
 function pc_add(obj){
 	var page_link = obj.getAttribute("value");
 	$.get(page_link, function(data){
-		$('#pcTable').children().remove();
-		$('#pcTable').html(data);
+		$('#mgt_content').children().remove();
+		$('#mgt_content').html(data);
+	});
+}
+function pc_add_submit(){
+	console.log($('#pcForm'));
+	//表单无内容则禁止提交
+	var pc_form_fields = $('#pcForm').serializeArray();
+	$('#pcForm').ajaxSubmit({
+		url:"/mgt/computer/add",
+		type:'post',
+		complete:function(data){
+			if(data.status == 200)
+				alert("添加电脑成功!");
+			else if(data.status == 400)
+				alert("请填入完整信息!");
+		}
 	});
 }
