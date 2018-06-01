08a310d81d6a982547693153b3d1ce28d82f517a
Jun <wangzhen_tju@126.com>
2018-06-02 06:27:38 +0800

    add cookie in controller

--- src/main/java/org/softcits/mgt/controller/MgtLoginController.java
+++ src/main/java/org/softcits/mgt/controller/MgtLoginController.java
@@ -1,5 +1,9 @@
 package org.softcits.mgt.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.softcits.utils.CookieUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,5 +19,11 @@ public class MgtLoginController {
 	@RequestMapping(path="/register",method=RequestMethod.GET)
 	public String goRegisterView() {
 		return "register";
+	}
+	
+	@RequestMapping(path="/setCookie",method=RequestMethod.GET)
+	public void setCookie(HttpServletRequest request, HttpServletResponse response) {
+		//写入cookie
+		CookieUtils.setCookie(request, response, "PC_TOKEN", "123456789abc");
 	}
 }
